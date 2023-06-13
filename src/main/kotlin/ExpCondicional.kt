85249e70040916758ec47b0aa4e6698da72d3eb8
Eduarda Clemente <eduardaclemente.369@gmail.com>
2023-06-13 16:22:12 -0300

    ternario e == or ===

--- src/main/kotlin/ExpCondicional.kt
+++ src/main/kotlin/ExpCondicional.kt
@@ -1,6 +1,15 @@
 fun main () {
 
-    fun max(a: Int, b: Int) = if (a > b) a else b         //
+    fun max(a: Int, b: Int) = if (a > b) a else b         // a nova
+
+    fun maxOld(a: Int, b: Int): Int {
+        if (a > b) {
+            return a
+        } else {
+            return b
+        }
+    }
 
     println(max(99, -42))
+    println(maxOld(99, -42))
 }
